ecfc3b92a404c3276384a4a800f6c132682bd71f
Seif Kamal <seif@tickx.co.uk>
2018-12-11 13:12:59 +0000

    Add search bot

---
+++ utils/Bot.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const google = require('google');
+
+class Bot {
+  constructor() {
+    this.google = google;
+  }
+
+  searchGoogle(query) {
+    let promise = new Promise((resolve, reject) => {
+      this.google(query, (err, next, links) => {
+        if (err) return reject(err);
+
+        let topFiveLinks = links.slice(0, 6);
+        resolve(topFiveLinks);
+      });
+    });
+
+    return promise;
+  }
+}
+
+module.exports = Bot;
