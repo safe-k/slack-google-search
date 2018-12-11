39c55292cb8ee13dccbc11784b643165944e5f4f
Seif Kamal <seif@tickx.co.uk>
2018-12-11 13:13:51 +0000

    Add updated app

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Bot = require('./utils/Bot');
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let bot = new Bot();
+
+app.post('/', (req, res) => {
+  if (req.body.token != process.env.SLACK_TOKEN) return res.sendStatus(401);
+
+  bot.searchGoogle(req.body.text).then((results) => {
+    let message = {
+      response_type: 'in_channel',
+      attachments: []
+    };
+
+    for (var result of results) {
+      let attachment = {
+        title: result.title,
+        title_link: result.href,
+        text: result.description
+      };
+
+      // Exclude dead links to Images, News, and Books
+      if(!/^Images/.test(result.title) && !/^News/.test(result.title) && !/^Books/.test(result.title)) {
+        message.attachments.push(attachment);
+      }
+    }
+
+    res.json(message);
+  })
+  .catch((err) => {
+    res.sendStatus(err);
+  });
+});
+
+let port = process.env.PORT;
+app.listen(port, () => {
+  console.log('Express app listening on port ' + port)
+});
