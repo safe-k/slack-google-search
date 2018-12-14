8524e7f7078013d68c36a040e6ba656d78136150
Seif Kamal <seif@tickx.co.uk>
2018-12-14 13:00:45 +0000

    Delay response to avoid timeout

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Bot = require('./utils/Bot');
+const request = require('request');
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,36 +12,69 @@ app.use(bodyParser.json());
 let bot = new Bot();
 
 app.post('/', (req, res) => {
-  if (req.body.token != process.env.SLACK_TOKEN) return res.sendStatus(401);
+    // Authenticate request
+    if (req.body.token != process.env.SLACK_TOKEN) return res.sendStatus(401);
 
-  bot.searchGoogle(req.body.text).then((results) => {
-    let message = {
-      response_type: 'in_channel',
-      attachments: []
-    };
-
-    for (var result of results) {
-      let attachment = {
-        title: result.title,
-        title_link: result.href,
-        text: result.description
-      };
-
-      // Exclude dead links to Images, News, and Books
-      if(!/^Images/.test(result.title) && !/^News/.test(result.title) && !/^Books/.test(result.title)) {
-        message.attachments.push(attachment);
-      }
+    // Check if text query was provided
+    if (!req.body.text) {
+        console.error('No search query provided.');
+        return res.send({
+            response_type: 'ephemeral',
+            text: 'What would you like me to search for?'
+        });
     }
 
-    res.json(message);
-  })
-  .catch((err) => {
-      console.log(err);
-      if (err.response) {
-          res.sendStatus(err.response.status);
-      }
-  });
+    // Acknowledge request before responding (this is to prevent Slack's 3s timeout)
+    res.send({
+        response_type: 'ephemeral',
+        text: 'Looking that up for you...'
+    });
+
+    return delayedResponse(req, res);
 });
+
+function delayedResponse(req, res) {
+    let message = {
+        response_type: 'ephemeral',
+        text: 'Sorry, that didn\'t work. Please try again.'
+    };
+
+    bot.searchGoogle(req.body.text).then((results) => {
+        let links = [];
+        for (let result of results) {
+            let link = {
+                title: result.title,
+                title_link: result.href,
+                text: result.description
+            };
+
+            // Exclude dead links to Images, News, and Books
+            if (!/^Images/.test(result.title) && !/^News/.test(result.title) && !/^Books/.test(result.title)) {
+                links.push(link);
+            }
+        }
+
+        message = {
+            response_type: 'in_channel',
+            attachments: links
+        };
+    }).catch((err) => {
+        console.error(err);
+        if (err.response) {
+            res.sendStatus(err.response.status);
+        }
+    }).finally(() => {
+        console.log('Message: ' + JSON.stringify(message));
+
+        request.post(req.body.response_url, { json: message }, (err, res, body) => {
+            console.log('statusCode: ' + res.statusCode);
+            console.log(body);
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+}
 
 let port = process.env.PORT;
 app.listen(port, () => {
